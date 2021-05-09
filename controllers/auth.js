a9ffdcb114cda1bca7f732ced9fc6ce18b0f988a
Jithesh Kumar <mulkijeetu2@gmail.com>
2021-05-09 12:04:47 +0530

    removed console

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,7 +3,7 @@ const { errorHandler } = require('../helpers/dbErrorHandler')
 const jwt = require('jsonwebtoken') // to generate signed token
 const expressJwt = require('express-jwt') // to check authorization
 exports.signup = (req, res) => {
-    console.log("req.body", req.body)
+    // console.log("req.body", req.body)
     const user = new User(req.body)
     user.save((err, user) => {
         if (err) {
